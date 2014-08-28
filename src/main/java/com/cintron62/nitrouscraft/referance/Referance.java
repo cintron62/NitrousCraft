3233265a7c81affead0395bfa65ea987088707bf
Matt <211074158delos>
2014-08-28 05:18:39 -0400

    version update

--- src/main/java/com/cintron62/nitrouscraft/referance/Referance.java
+++ src/main/java/com/cintron62/nitrouscraft/referance/Referance.java
@@ -4,7 +4,7 @@ public class Referance
 {
     public static final String MOD_ID = "NitrousCraft";
     public static final String MOD_NAME = "Nitrous Craft";
-    public static final String VERSION = "1.7.10-0.0.1";
+    public static final String VERSION = "1.7.10-0.0.2";
     public static final String CLIENT_PROXY_CLASS = "com.cintron62.nitrouscraft.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.cintron62.nitrouscraft.proxy.ServerProxy";
     public static final String GUI_FACTORY_CLASS = "com.cintron62.nitrouscraft.client.gui.GuiFactory";
